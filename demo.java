8063a28080e9a147be6e826ab3467383e275437a
Tarannum <abc.com>
2022-12-18 12:13:01 +0530

    File commit

---
+++ demo.java
@@ -0,0 +1,12 @@
+public class Demo {
+
+	public class Demo1 {
+
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Assignment from Feature2");
+	}
+
+}
